6e5a3eb9fb89103b0b3373321ff8861edfcca3d9
EstherWaweru <estherwaweru86@gmail.com>
2021-06-11 10:00:27 +0300

    Testing with sql db

--- ecommerce_backend/settings/travis.py
+++ ecommerce_backend/settings/travis.py
@@ -9,14 +9,14 @@ ALLOWED_HOSTS = []
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         # sqlite database
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+DATABASES = {
+    'default': {
+        # sqlite database
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
 
-#     }
-# }
+    }
+}
 
 
 #EMAIL CONFIGURATIONS
